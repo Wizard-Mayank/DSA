ce3a90edcaf7281f8fcfcb32c52a1fe57d87b8c9
Wizard-Mayank <30517csaiml@gmail.com>
2024-01-27 19:42:08 +0530

    Implement level-order traversal of a binary tree in two different ways.

--- binary-tree-traversals.c
+++ binary-tree-traversals.c
@@ -1,18 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-//                              Defining node structure
-typedef struct Node
+//                              Defining tNode structure
+typedef struct btNode
 {
-    struct Node *left;
+    struct btNode *left;
     int data;
-    struct Node *right;
-} node;
+    struct btNode *right;
+} tNode;
 
-//                              Creating a new node
-node *createNode(int data)
+//                              Defining linkedNode structure
+typedef struct linkedNode
 {
-    node *new = malloc(sizeof(node));
+    tNode *data;
+    struct linkedNode *next;
+} lNode;
+
+//                              Defining Queue structure
+typedef struct Queue
+{
+    lNode *front;
+    lNode *rear;
+} queue;
+
+//                              Queue isEmpty()
+
+bool isEmpty(queue *Q)
+{
+    if (Q->front == NULL)
+        return true;
+    else
+        return false;
+}
+
+//                              Insert operation in queue
+void enqueue(queue *Q, tNode *new)
+{
+    lNode *temp = (lNode *)malloc(sizeof(lNode));
+
+    temp->data = new;
+    temp->next = NULL;
+
+    if (Q->front == NULL)
+        Q->front = temp;
+
+    if (Q->rear == NULL)
+        Q->rear = temp;
+    
+    else
+    {
+        Q->rear->next = temp;
+        Q->rear = Q->rear->next;
+    }
+}
+
+//                              Delete Operation in queue
+tNode *dequeue(queue *Q)
+{
+    if (isEmpty(Q))
+        return NULL;
+    
+    tNode *new = Q->front->data;
+
+    if (Q->front == Q->rear)
+    {
+        Q->front = Q->rear = NULL;
+    }
+
+    else
+    {
+        Q->front = Q->front->next;
+    }
+
+    return (new);
+}
+
+//                              Creating a new tNode
+tNode *createNode(int data)
+{
+    tNode *new = malloc(sizeof(tNode));
 
     new->left = NULL;
     new->data = data;
@@ -22,7 +89,7 @@ node *createNode(int data)
 }
 
 //                              Pre-Order Traversal
-void preOrder(node *root)
+void preOrder(tNode *root)
 {
     if (root != NULL)
     {
@@ -33,7 +100,7 @@ void preOrder(node *root)
 }
 
 //                              In-Order Traversal
-void inOrder(node *root)
+void inOrder(tNode *root)
 {
     if (root != NULL)
     {
@@ -44,7 +111,7 @@ void inOrder(node *root)
 }
 
 //                              Post-Order Traversal
-void postOrder(node *root)
+void postOrder(tNode *root)
 {
     if (root != NULL)
     {
@@ -54,45 +121,67 @@ void postOrder(node *root)
     }
 }
 
-//                              Level-Order Traversal
-void currentLevel(node *root, int level)
+//                              Level-Order Traversal - 1
+void currentLevel(tNode *root, int level)
 {
-    if (root==NULL)
+    if (root == NULL)
         return;
-    else if (level==1)
+    else if (level == 1)
         printf("%d -> ", root->data);
-    else{
-        currentLevel(root->left, level-1);
-        currentLevel(root->right, level-1);
+    else
+    {
+        currentLevel(root->left, level - 1);
+        currentLevel(root->right, level - 1);
+    }
+}
+
+//                              Level-Order Traversal - 2
+
+void levelOrder(tNode *root)
+{
+    queue *Q = (queue*)malloc(sizeof(queue));
+    Q->front = NULL;
+    Q->rear = NULL;
+
+    enqueue(Q, root);
+
+    while (!isEmpty(Q))
+    {
+        tNode *data = dequeue(Q);
+
+        if (data==NULL)
+            break;
+
+        printf("%d -> ", data->data);
+
+        if (data->left != NULL)
+            enqueue(Q, data->left);
+
+        if (data->right != NULL)
+            enqueue(Q, data->right);
+
+        free(data);
     }
 }
 
 int main()
 {
-    node *root, *p1, *p2, *p3, *p4, *p5, *p6;
+    tNode *root, *p1, *p2, *p3, *p4, *p5, *p6;
 
-    root = malloc(sizeof(node));
-    p1 = malloc(sizeof(node));
-    p2 = malloc(sizeof(node));
-    p3 = malloc(sizeof(node));
-    p4 = malloc(sizeof(node));
-    p5 = malloc(sizeof(node));
-    p6 = malloc(sizeof(node));
-
-    root = (node *)createNode(1);
-    p1 = (node *)createNode(2);
-    p2 = (node *)createNode(3);
-    p3 = (node *)createNode(4);
-    p4 = (node *)createNode(5);
-    p5 = (node *)createNode(6);
-    p6 = (node *)createNode(7);
+    root = (tNode *)createNode(1);
+    p1 = (tNode *)createNode(2);
+    p2 = (tNode *)createNode(3);
+    p3 = (tNode *)createNode(4);
+    p4 = (tNode *)createNode(5);
+    p5 = (tNode *)createNode(6);
+    p6 = (tNode *)createNode(7);
 
     root->left = p1;
     root->right = p2;
     p1->left = p3;
     p1->right = p4;
-    p2->left=p5;
-    p2->right=p6;
+    p2->left = p5;
+    p2->right = p6;
 
     printf("Pre-Order Traversal: ");
     preOrder(root);
@@ -106,9 +195,13 @@ int main()
     postOrder(root);
     printf("NULL\n\n");
 
-    printf("Level-Order Traversal: ");
-    for(int i=1; i<=2; ++i)
+    printf("Level-Order Traversal - 1: ");
+    for (int i = 1; i <= 3; ++i)
         currentLevel(root, i);
+    printf("NULL\n\n");
+
+    printf("Level-Order Traversal - 2: ");
+    levelOrder(root);
     printf("NULL\n\n");
 
     return 0;
